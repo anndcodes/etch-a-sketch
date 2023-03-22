43fd1b03853124212abf9db0144f8cfdaf6f3b0f
Annd <ssandreia30@gmail.com>
2023-03-22 17:02:47 -0300

    feat: add hover effect when mouse passes over grids

--- script.js
+++ script.js
@@ -4,4 +4,8 @@ for(let i = 0; i < 256; i++) {
   let grids = document.createElement("div");
   container.appendChild(grids);
   grids.classList.add("grids");
+
+  grids.addEventListener('mouseover', () => {
+    grids.style.background = "#000";
+  } )
 }
