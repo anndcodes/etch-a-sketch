9c70fb864d33db01d921469d24f6b65e09831cb5
Annd <ssandreia30@gmail.com>
2023-03-22 16:06:02 -0300

    feat: create a 16x16 grid using JavaScript and flexbox

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".container");
+
+for(let i = 0; i < 256; i++) {
+  let grids = document.createElement("div");
+  container.appendChild(grids);
+  grids.classList.add("grids");
+}
