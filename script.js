09bb97febc6ede7b9982fe4a1a201e6c9ad7407c
Annd <ssandreia30@gmail.com>
2023-03-23 21:58:47 -0300

    feat: create function to make grids, clear the sketch pad and make more grids with the giving value

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 const container = document.querySelector(".container");
+const gridSize = document.querySelector(".grid-size");
 
-for(let i = 0; i < 256; i++) {
-  let grids = document.createElement("div");
-  container.appendChild(grids);
-  grids.classList.add("grids");
+// ------ function to make grids ------- //
+function gridMaker(grid) {
+  for(let i = 0; i < grid * grid; i++) {
+    let grids = grid <= 100 ? document.createElement("div") : false;
+    container.appendChild(grids);
+    grids.style.width = `calc(60rem/${grid})`;
+    grids.style.height = `(calc(60rem/${grid})`;
+  
+    grids.classList.add("grids");
 
-  grids.addEventListener('mouseover', () => {
-    grids.style.background = "#000";
-  } )
+    grids.addEventListener('mouseover', () => {
+      grids.style.background = "#000";
+    } )
+  }
 }
+  
+gridMaker(16);
+
+// -------- function to clear sketch pad and create new grids with the giving value -------- // 
+function moreGrids() {
+  gridSize.addEventListener("keypress", function(e) {
+    if(e.key === "Enter") {
+      let allGrids = Array.from(document.querySelectorAll(".grids"));
+      // console.log(typeof(allGrids));
+      // console.log(allGrids);
+      allGrids.forEach(grids => {
+        grids.remove();
+      });
+      
+      let gridSizeValue = parseInt(gridSize.value);
+      // console.log(gridSizeValue);
+      gridMaker(gridSizeValue);
+    }
+  })
+}
+
+moreGrids()
